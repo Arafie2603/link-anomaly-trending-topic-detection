2097f692ced563d764df223c63484e7c24bb6284
Arafie2603 <arrafiesetiawan@gmail.com>
2024-10-18 21:00:31 +0700

    data tweets sampel

---
+++ data/tweets.py
@@ -0,0 +1,51 @@
+
+tweets_data = [
+    {
+        "No": 1,
+        "Time": "23 Juni 2016 10:26:08 AM",
+        "User Twitter": "NHasan03",
+        "Tweet": "Ahok modus manipulasi ktp @Fdayun @Faktababelcom",
+        "K": 2,
+        "V": ["@Fdayun", "@Faktababelcom"]
+    },
+    {
+        "No": 2,
+        "Time": "23 Juni 2016 10:26:12 AM",
+        "User Twitter": "Fdayun",
+        "Tweet": "Ahok modus pembuatan sertifikat @Faktababelcom",
+        "K": 1,
+        "V": ["@Faktababelcom"]
+    },
+    {
+        "No": 3,
+        "Time": "23 Juni 2016 10:26:13 AM",
+        "User Twitter": "Faktababelcom",
+        "Tweet": "Jokowi natuna ratas imam bonjol @FaktaSumbarcom",
+        "K": 1,
+        "V": ["@FaktaSumbarcom"]
+    },
+    {
+        "No": 4,
+        "Time": "23 Juni 2016 10:26:14 AM",
+        "User Twitter": "FaktaSumbarcom",
+        "Tweet": "Jokowi rapat natuna @Faktababelcom @NHasa03",
+        "K": 2,
+        "V": ["@Faktababelcom", "@NHasa03"]
+    },
+    {
+        "No": 5,
+        "Time": "23 Juni 2016 10:26:16 AM",
+        "User Twitter": "NHasan03",
+        "Tweet": "TNI jamin keamanan jokowi natuna @FaktaSumbarcom",
+        "K": 1,
+        "V": ["@FaktaSumbarcom"]
+    },
+    {
+        "No": 6,
+        "Time": "23 Juni 2016 10:26:24 AM",
+        "User Twitter": "Nurul",
+        "Tweet": "Ahok modus manipulasi ktp @Anisakha007",
+        "K": 1,
+        "V": ["@Anisakha007"]
+    }
+]
