2f6c5250863199836a3a7145a027d7954c4246d4
Arafie2603 <arrafiesetiawan@gmail.com>
2024-10-22 16:23:11 +0700

    integrasi database ✨

---
+++ utils/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+# Memuat variabel dari file .env
+load_dotenv()
+
+class Config:
+    DATABASE_CONFIG = {
+        'user': os.getenv('DB_USERNAME'),
+        'password': os.getenv('DB_PASSWORD'),
+        'host': os.getenv('DB_HOST'),
+        'database': os.getenv('DB_NAME'),
+        'raise_on_warnings': True
+    }
