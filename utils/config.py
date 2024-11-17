bf393e30ede391736b950d1f40375ecdef2d959b
Arafie2603 <arrafiesetiawan@gmail.com>
2024-11-17 14:18:02 +0700

    modifikasi beberapa config aja

--- utils/config.py
+++ utils/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     DATABASE_CONFIG = {
-        'user': os.getenv('DB_USERNAME'),
+        'user': os.getenv('DB_USER'),
         'password': os.getenv('DB_PASSWORD'),
         'host': os.getenv('DB_HOST'),
         'database': os.getenv('DB_NAME'),
